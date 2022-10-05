9504afcbfe3c8f24cbf02d9e46f66a443b962db2
Anna.Sadykova@urfu.me <Anna.Sadykova@urfu.me>
2022-10-05 19:35:57 +0500

    Выполнено дз 1

---
+++ task1/task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Сергей Александрович Есенин");
+            Console.ForegroundColor= ConsoleColor.Blue;
+            Console.WriteLine(" Не жалею, не зову, не плачу...");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n Не жалею, не зову, не плачу,\n Все пройдет, как с белых яблонь дым.");
+            Console.ResetColor();
+
+            Console.ReadKey();
+
+        }
+    }
+}
