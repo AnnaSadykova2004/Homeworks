238fd4af3527024d03e00d9ec05d0a81580c4c96
Anna.Sadykova@urfu.me <Anna.Sadykova@urfu.me>
2022-10-13 15:32:53 +0500

    дз 4 выполнено

---
+++ task04/task04/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение x"); 
+            double x = double.Parse(Console.ReadLine());
+
+            double y = (Math.Abs(x) + Math.Sin(Math.Pow((Math.Tan(x)), 2) + 4)) / (5.5 * x);
+            Console.WriteLine("y = f(x), y = " + y);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
