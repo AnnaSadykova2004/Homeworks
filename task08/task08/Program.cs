034a30c787360a286b86bbfca8108e5ccad257d9
Anna.Sadykova@urfu.me <Anna.Sadykova@urfu.me>
2022-11-20 11:21:20 +0500

    дз 8 решено

---
+++ task08/task08/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Введите значение х");
+           double x = double.Parse(Console.ReadLine());
+           Console.WriteLine(Calc(x));
+        }
+        static double Calc(double x)
+        {
+            if (x > 1)
+            {
+                return 2 - x;
+            }
+            else if (x >= -1 & x <= 1)
+            {
+                return x * x;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+                  
+    }
+}
