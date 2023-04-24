2abeee3afe91bed7ed0d645f6b9d25bb693f6a12
Anna.Sadykova@urfu.me <Anna.Sadykova@urfu.me>
2023-04-24 17:12:14 +0500

    Работа выполнена

--- Hospital/Hospital.UnitTests/PatientTests.cs
+++ Hospital/Hospital.UnitTests/PatientTests.cs
@@ -1,31 +1,121 @@
+using System.Reflection;
+
 namespace Hospital.UnitTests_
 {
     [TestFixture]
-    public class PersonTests
+    public class PatientTests
     {
         static Patient patient;
 
         [SetUp]
         public void Setup()
         {
-            patient = new Patient("Peter", "Ivanov", "1378 6523 7060 5432");
+            patient = new Patient("Петр", "Иванов", "1378 6523 7060 5432");
+            
         }
 
         [Test]
-        public void ConstructotTest()
+        public void ConstructorTest()
         {
-            Assert.That(patient.Name, Is.EqualTo("Peter"));
-            Assert.That(patient.Surname, Is.EqualTo("Ivanov"));
+            Assert.That(patient.Name, Is.EqualTo("Петр"));
+            Assert.That(patient.Surname, Is.EqualTo("Иванов"));
             Assert.That(patient.PoliceNumber, Is.EqualTo("1378 6523 7060 5432"));
         }
 
         [Test]
         public void GetInfo_Patient_ValueString()
         {
-            string expectedInfo = "Peter Ivanov. ";
-            expectedInfo += "Номер полиса: 1378 6523 7060 5432.";
+            string expectedInfo = "Петр Иванов. ";
+            expectedInfo += "Номер полиса:1378 6523 7060 5432.";
 
             Assert.That(patient.GetInfo(), Is.EqualTo(expectedInfo));
+        }
+    }
+
+    [TestFixture]
+    public class HospitalPatientTests
+    {
+        HospitalPatient man1;
+
+        [SetUp]
+        public void Setup()
+        {
+            man1 = new HospitalPatient("Петр", "Иванов", "1378 6523 7060 5432", "физиотерапевтического", 105);
+
+        }
+
+        [Test] 
+        public void ConstructorHPTest()
+        { 
+            Assert.That(man1.Department, Is.EqualTo("физиотерапевтического")); 
+        }
+
+        [Test] 
+        public void GetInfoTest() 
+        {
+            var expected = "Петр Иванов. " +
+            $"Номер полиса:1378 6523 7060 5432." +
+            "\nПациент стационара из физиотерапевтического отделения.";
+          
+            Assert.That(man1.GetInfo(), Is.EqualTo(expected));
+        }
+    }
+
+    public class DayHospitalPatientTests
+    {
+        DayHospitalPatient man2;
+
+        [SetUp]
+        public void Setup()
+        {
+            man2 = new DayHospitalPatient("Петр", "Иванов", "1378 6523 7060 5432", DateTime.Parse("01.04.2023 12:00:00"), DateTime.Parse("01.04.2023 14:00:00"));
+
+        }
+
+        [Test]
+        public void ConstructorDHPTest()
+        {
+            Assert.That(man2. ArrivalTime, Is.EqualTo(DateTime.Parse("01.04.2023 12:00:00")));
+            Assert.That(man2.LeavingTime, Is.EqualTo(DateTime.Parse("01.04.2023 14:00:00")));
+        }
+
+        [Test]
+        public void GetInfoTest()
+        {
+            var expected = "Петр Иванов. " +
+            $"Номер полиса:1378 6523 7060 5432." +
+            "\nПациент дневного стационара пришел 01.04.2023 12:00:00." +
+            "\nУшел домой 01.04.2023 14:00:00.";
+
+            Assert.That(man2.GetInfo(), Is.EqualTo(expected));
+        }
+    }
+
+    public class AmbulatoryPatientTests
+    {
+        AmbulatoryPatient man3;
+
+        [SetUp]
+        public void Setup()
+        {
+            man3 = new AmbulatoryPatient("Петр", "Иванов", "1378 6523 7060 5432", "Сафронова Дмитрия Романовича");
+
+        }
+
+        [Test]
+        public void ConstructorAPTest()
+        {
+            Assert.That(man3.DoctorInitials, Is.EqualTo("Сафронова Дмитрия Романовича"));
+        }
+
+        [Test]
+        public void GetInfoTest()
+        {
+            var expected = "Петр Иванов. " +
+            $"Номер полиса:1378 6523 7060 5432." +
+            "\nАмбулаторный пациент лечится у Сафронова Дмитрия Романовича."; 
+            
+            Assert.That(man3.GetInfo(), Is.EqualTo(expected));
         }
     }
 }
--- Hospital/Hospital/Patient.cs
+++ Hospital/Hospital/Patient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,22 +11,62 @@ namespace Hospital
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public readonly int PoliceNumber;
+        public readonly string PoliceNumber;
         public DateTime AdmissionDate;
         public DateTime DischargeDate;
         public Service Service;
         public int TreatmentCost;
 
-        public Patient(string name, string surname, int policeNumber)
+        public Patient(string name, string surname, string policeNumber)
         {
             Name = name;
             Surname = surname;
             PoliceNumber = policeNumber;
         }
-
-        public string GetInfo()
+        public virtual string GetInfo()
         {
-            return $"{Name} {Surname}. Номер полиса:{PoliceNumber}.";
+           return $"{Name} {Surname}. Номер полиса:{PoliceNumber}.";
         }
+    }
+    public class HospitalPatient : Patient 
+    {
+        public string Department { get; set; }
+        public int RoomNumber { get; set; }
+
+        public HospitalPatient(string name, string surname, string policeNumber, string department, int roomNumber) : base(name, surname, policeNumber)
+        {
+            Department = department;
+            RoomNumber = roomNumber;
+        }
+
+        public override string GetInfo() => base.GetInfo() +
+        $"\nПациент стационара из {Department} отделения.";
+    }
+    public class DayHospitalPatient : Patient 
+    {
+        public DateTime ArrivalTime { get; set; }
+        public DateTime LeavingTime { get; set; }
+
+        public DayHospitalPatient(string name, string surname, string policeNumber, DateTime arrivalTime, DateTime leavingTime) : base(name, surname, policeNumber)
+        {
+            ArrivalTime = arrivalTime;
+            LeavingTime = leavingTime;
+        }
+
+        public override string GetInfo() => base.GetInfo() +
+        $"\nПациент дневного стационара пришел {ArrivalTime}." +
+        $"\nУшел домой {LeavingTime}.";
+    }
+    public class AmbulatoryPatient : Patient
+    {
+        public string DoctorInitials { get; set; }
+        
+        public AmbulatoryPatient(string name, string surname, string policeNumber, string doctorInitials) : base(name, surname, policeNumber)
+        {
+            DoctorInitials = doctorInitials;
+        }
+
+        public override string GetInfo() => base.GetInfo() +
+        $"\nАмбулаторный пациент лечится у {DoctorInitials}.";
     }
 }
